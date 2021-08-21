31cdca699fe1f771553f7cbce5f591741b506ebb
dinushchathurya <dinushchathurya21@gmail.com>
2021-08-21 17:13:41 +0530

    :wrench: Create UserActivity middleware

---
+++ app/Http/Middleware/UserActivity.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class UserActivity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {   
+        if (Auth::check()) {
+            $expiresAt = now()->addMinutes(2); /* keep online for 2 min */
+            Cache::put('user-is-online-' . Auth::user()->id, true, $expiresAt);
+  
+            /* last seen */
+            User::where('id', Auth::user()->id)->update(['last_seen' => now()]);
+        }
+        return $next($request);
+    }
+}
