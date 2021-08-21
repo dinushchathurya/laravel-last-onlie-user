b5dd3e55611f265d02ca4963648ba7fcf3d681eb
dinushchathurya <dinushchathurya21@gmail.com>
2021-08-21 17:30:21 +0530

    :sparkles: Create UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        $users = User::select("*")
+            ->whereNotNull('last_seen')
+            ->orderBy('last_seen', 'DESC')
+            ->paginate(10);
+        return view('users', compact('users'));
+    }
+}
